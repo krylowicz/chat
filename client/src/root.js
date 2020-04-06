ac7cef717595f27b93b8b364a996d15d29670683
Kacper <kacper@krylowicz.pl>
2020-04-06 19:58:51 +0200

    Revert "♻️refactor"

--- client/src/root.js
+++ client/src/root.js
@@ -1,22 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { UserContextProvider } from 'context/userContext';
-import Login from 'components/Login/Login';
-import Register from 'components/Register/Register';
-import Chat from 'components/Chat/Chat';
 
-const Root = () => {
-  return (
-    <Router>
-      <UserContextProvider>
-        <Switch>
-          <Route exact path="/" component={Chat} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </UserContextProvider>
-    </Router>
-  );
-};
+const Root = () => <h1>hello world</h1>;
 
 export default Root;
